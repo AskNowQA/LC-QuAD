ace6de3bc6b527d9b2a914b1595090368e2c2b1e
Priyansh Trivedi <pc.priyansh@gmail.com>
2016-09-21 18:55:17 +0200

    Dirty Commit. Day ends. Will refactor, beautify and document tomorrow.

--- utils/dbpedia_interface.py
+++ utils/dbpedia_interface.py
@@ -11,6 +11,7 @@
 	A: Because I can, bitch.
 '''
 from SPARQLWrapper import SPARQLWrapper, JSON
+from pprint import pprint
 import traceback
 import warnings
 import random
@@ -19,18 +20,19 @@ import random
 import natural_language_utilities as nlutils
 
 #GLOBAL MACROS
-DBPEDIA_ENDPOINTS = ['http://dbpedia.org/sparql/','http://de.dbpedia.org/sparql/','http://live.dbpedia.org/sparql/']
+DBPEDIA_ENDPOINTS = ['http://dbpedia.org/sparql/','http://live.dbpedia.org/sparql/']
 MAX_WAIT_TIME = 1.0
 
 #SPARQL Templates
-GET_PROPERTIES_OF_RESOURCE = '''SELECT DISTINCT ?property WHERE { %(target_resource)s ?property ?useless_resource }
-'''
+GET_PROPERTIES_OF_RESOURCE = '''SELECT DISTINCT ?property WHERE { %(target_resource)s ?property ?useless_resource }'''
 
 GET_PROPERTIES_OF_RESOURCE_WITH_OBJECTS = '''SELECT DISTINCT ?property ?resource WHERE { %(target_resource)s ?property ?resource	}'''
 
+GET_ENTITIES_OF_CLASS = '''SELECT DISTINCT ?entity WHERE {	?entity <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> %(target_class)s } '''
+
 GET_LABEL_OF_RESOURCE = '''SELECT DISTINCT ?label WHERE { %(target_resource)s <http://www.w3.org/2000/01/rdf-schema#label> ?label . FILTER (lang(?label) = 'en')	} '''
 
-GET_ENTITIES_OF_CLASS = ''' SELECT DISTINCT ?entity WHERE {	?entity <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> %(target_class)s } '''
+GET_TYPE_OF_RESOURCE = '''SELECT DISTINCT ?type WHERE { %(target_resource)s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type } '''
 
 class DBPedia:
 	def __init__(self,_method='round-robin',_verbose=False):
@@ -50,10 +52,9 @@ class DBPedia:
 			This function is to be called whenever we're making a call to DBPedia. Based on the selection mechanism selected at __init__,
 			this function tells which endpoint to use at every point.
 		'''
-		
 		if self.selection_method == 'round-robin':
 			index = DBPEDIA_ENDPOINTS.index(self.sparql_endpoint)
-			return DBPEDIA_ENDPOINTS[index+1] if index < len(DBPEDIA_ENDPOINTS) else DBPEDIA_ENDPOINTS[0]
+			return DBPEDIA_ENDPOINTS[index+1] if index >= len(DBPEDIA_ENDPOINTS) else DBPEDIA_ENDPOINTS[0]
 
 		if self.selection_method == 'select-one':
 			return self.sparql_endpoint
@@ -70,14 +71,13 @@ class DBPedia:
 				if _with_connected_resource == True, [ (R,O), (R,O), (R,O) ...]
 				else [ R,R,R,R...]
 		'''
-
 		#Check if the resource URI is shorthand or a proper URI
 		if not nlutils.has_url(_resource_uri):
 			warnings.warn("The passed resource %s is not a proper URI but is in shorthand. This is strongly discouraged." % _resource_uri)
 			_resource_uri = nlutils.convert_shorthand_to_uri(_resource_uri)
 
 		#Prepare the SPARQL Request
-		sparql = SPARQLWrapper(self.sparql_endpoint)
+		sparql = SPARQLWrapper(self.select_sparql_endpoint())
 		# with SPARQLWrapper(self.sparql_endpoint) as sparql:
 		if _with_connected_resource:
 			_resource_uri = '<'+_resource_uri+'>'
@@ -100,6 +100,7 @@ class DBPedia:
 
 		return property_list
 
+
 	def get_entities_of_class(self, _class_uri):
 		'''
 			This function can fetch the properties connected to the class passed as a function parameter _class_uri.
@@ -113,7 +114,7 @@ class DBPedia:
 			_class_uri = nlutils.convert_shorthand_to_uri(_class_uri)
 
 		#Preparing the SPARQL Query
-		sparql = SPARQLWrapper(self.sparql_endpoint)
+		sparql = SPARQLWrapper(self.select_sparql_endpoint())
 		# with SPARQLWrapper(self.sparql_endpoint) as sparql:
 		_class_uri = '<'+_class_uri+'>'
 		sparql.setQuery(GET_ENTITIES_OF_CLASS % {'target_class':_class_uri} )
@@ -130,8 +131,107 @@ class DBPedia:
 		return entity_list
 
 
+	def get_type_of_resource(self, _resource_uri, _filter_dbpedia = False):
+		'''
+			Function fetches the type of a given entity
+			and can optionally filter out the ones of DBPedia only
+		'''
+
+		if not nlutils.has_url(_resource_uri):
+			warnings.warn("The passed resource %s is not a proper URI but probably a shorthand. This is strongly discouraged." % _resource_uri)
+			_resource_uri = nlutils.convert_shorthand_to_uri(_resource_uri)
+
+		#Perparing the SPARQL Query
+		sparql = SPARQLWrapper(self.select_sparql_endpoint())
+		_resource_uri = '<'+_resource_uri+'>'
+		sparql.setQuery(GET_TYPE_OF_RESOURCE % {'target_resource': _resource_uri} )
+		sparql.setReturnFormat(JSON)
+		response = sparql.query().convert()
+
+		try:
+			type_list = [ x[u'type'][u'value'].encode('ascii','ignore') for x in response[u'results'][u'bindings'] ]
+		except:
+			traceback.print_exc()
 
 
 
+		#If we need only DBPedia's types
+		if _filter_dbpedia:
+			filtered_type_list = [x for x in type_list if x[:28] in ['http://dbpedia.org/ontology/','http://dbpedia.org/property/'] ]
+			return filtered_type_list
+
+		return type_list
+
+	def get_answer(self, _sparql_query):
+		'''
+			Function used to shoot a query and get the answers back. Easy peasy.
+
+			Return - array of values of first variable of query
+			NOTE: Only give it queries with one variable
+		'''
+
+		sparql = SPARQLWrapper(self.select_sparql_endpoint())
+		sparql.setQuery(_sparql_query)
+		sparql.setReturnFormat(JSON)
+		try:
+			response = sparql.query().convert()
+		except:
+			traceback.print_exc()
+
+		#Now to parse the response
+		variables = [x for x in response[u'head'][u'vars']]
+
+		#NOTE: Assuming that there's only one variable
+		value = [ x[variables[0]][u'value'].encode('ascii','ignore') for x in response[u'results'][u'bindings'] ]
+
+		return value
+
+	def get_label(self, _resource_uri):
+		'''
+			Function used to fetch the english label for a given resource.
+			Not thoroughly tested tho.
+
+			Always returns one value
+		'''
+
+		if not nlutils.has_url(_resource_uri):
+			warnings.warn("The passed resource %s is not a proper URI but probably a shorthand. This is strongly discouraged." % _resource_uri)
+			_resource_uri = nlutils.convert_shorthand_to_uri(_resource_uri)
+
+		#Preparing the Query
+		sparql = SPARQLWrapper(self.select_sparql_endpoint())
+		_resource_uri = '<'+_resource_uri+'>'
+		sparql.setQuery(GET_LABEL_OF_RESOURCE % {'target_resource': _resource_uri} )
+		sparql.setReturnFormat(JSON)
+		try:
+			response = sparql.query().convert()
+		except:
+			traceback.print_exc()
+
+		#Parsing the results
+		try:
+			results = [x[u'label'][u'value'].encode('ascii','ignore') for x in response[u'results'][u'bindings'] ]
+		except:
+			traceback.print_exc()
+
+		return results[0]
 
 
+
+if __name__ == '__main__':
+	pass
+	# print "\n\nBill Gates"
+	# pprint(dbp.get_type_of_resource('http://dbpedia.org/resource/Bill_Gates', _filter_dbpedia = True))
+	# print "\n\nIndia"
+	# pprint(dbp.get_type_of_resource('http://dbpedia.org/resource/India', _filter_dbpedia = True))
+
+	# q = 'SELECT DISTINCT ?uri WHERE { ?uri <http://dbpedia.org/ontology/birthPlace> <http://dbpedia.org/resource/Mengo,_Uganda> . ?uri <http://dbpedia.org/ontology/birthPlace> <http://dbpedia.org/resource/Uganda> }'
+	# pprint(dbp.get_answer(q))
+
+	# dbp = DBPedia()
+
+	# q = 'http://dbpedia.org/ontology/birthPlace'
+	# pprint(dbp.get_label(q))
+
+	# q = 'http://dbpedia.org/resource/Bill_Gates'
+	# pprint(dbp.get_label(q))
