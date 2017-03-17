fd9b46fd87555a2376145e066bc6a0bc810c3353
gaurav <saist1993@gmail.com>
2017-03-17 21:17:31 +0100

    :banana: Copied the content of ipny file to a py file . MAkes it easier to collabrate. template verbelizer 3 verblizes template 3 :trollface:  :skull:  :alien:

---
+++ template3_verbalizer.py
@@ -0,0 +1,24 @@
+#parsing code
+
+import json
+from pprint import pprint
+import natural_language_utilities as nlutils
+
+with open('template3.json') as data_file:    
+    data = json.load(data_file)
+questions = []
+question_format = "What is the <%(e_in_to_e)s> of the <%(x)s> who is the <%(e_in_in_to_e_in)s> of <%(e_in_in)s> ?"
+for filler in data:
+	x = filler["answer_type"]['x']
+	maps = filler['mapping']
+	maps['x'] = x
+	#replace each path by label
+	for element in maps:
+		maps[element] = nlutils.get_label_via_parsing(maps[element])	#obtain just elements. 
+	questions.append(question_format % maps)
+
+for question in questions:
+	print question
+
+print "*******************"
+print len(questions)
