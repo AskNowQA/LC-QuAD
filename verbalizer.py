c41c6ce42d976fe6396cfda69d7bab3555d919e9
Priyansh Trivedi <pc.priyansh@gmail.com>
2017-04-17 00:09:49 +0200

    The verbalizer now functions. Tested with template 3. Template 3 verbalizer itself is now depriciated, and is deleted from the current code

--- verbalizer.py
+++ verbalizer.py
@@ -7,14 +7,22 @@
 		However, there was a lot of code repetition and while changing anything, things could go really really bad. 
 
 	This class is thus responsible for taking data from the output folder, and verbalizing it, based on what kind of template it is. 
+	NOTE: this is an abstract class and will be used in different places
 
 '''
+
+import csv
+import json
+import numpy as np
+from pprint import pprint
+from pattern.en import pluralize
+import utils.natural_language_utilities as nlutils
 
 class Verbalizer:
 
 	question_templates = {}		#Placeholder to keep all the templates here. In the form key: list, key:list
-
-	def __init__(self, _template_id, _has_x = False, _has_uri = True, _template_id_offset = 0, _template_type = 'Vanilla'):
+	
+	def __init__(self, _template_id, _has_x = False, _has_uri = False, _template_id_offset = 0, _template_type = 'Vanilla'):
 		'''
 			Parameters:
 				template_id: the template ID (corresponds to the file from where we pick sparqls. Corresponds to ID in templates.py)
@@ -33,17 +41,19 @@ class Verbalizer:
 		sparqls = []			#Holds the un-verbalized data
 		questions = []			#Holds the verbalized data
 
-		with open('output/template%s.txt' % _template_id) as data_file:
+		self.hard_relation_filter_map = {} 		#Keeps count of relations and limits. (Used in filter class)
+
+		with open('sparqls/template%s.txt' % _template_id) as data_file:
 			for line in data_file:
 				sparqls.append(json.loads(line.rstrip('\n')))
 
 				
-		for counter in range(len(data)):
+		for counter in range(len(sparqls)):
 
 			#Declate the verbalized flag
-			data[counter]['verbalized'] = False
+			sparqls[counter]['verbalized'] = False
 
-			datum = data[counter]
+			datum = sparqls[counter]
 			maps = datum["mapping"]
 
 			if _has_uri:
@@ -60,41 +70,47 @@ class Verbalizer:
 				except:
 					continue
 
+			#Convert the URIs to their corresponding labels
+			#@TODO: Use dbpedia labels to do this.
+			for element in maps:
+				maps[element] = nlutils.get_label_via_parsing(maps[element], lower = True)  #Get their labels
+
+
 			#See if you want this filtered
-			if filter(_datum = datum, _maps = maps):
+			if not self.filter(_datum = datum, _maps = maps):
 				#Don't bother verbalizing this
 				continue
 
 			#Select a template for this question
-			maps, question_format = rules(_maps = maps,_datum = datum)
+			maps, question_format = self.rules(_maps = maps,_datum = datum)
 
-			data[counter]['verbalized_question'] = question_format % maps
-			data[counter]['verbalized'] = True
-			data[counter]['id'] = _template_id + _template_id_offset
-			data[counter]['type'] = _template_type
+			sparqls[counter]['verbalized_question'] = question_format % maps
+			sparqls[counter]['verbalized'] = True
+			sparqls[counter]['id'] = _template_id + _template_id_offset
+			sparqls[counter]['type'] = _template_type
 
 		#Writing everything to file
 		id = _template_id + _template_id_offset
 		fo = open('output/verbalized_template%s.txt' % id, 'w+')
-		for datum in data:
+		for datum in sparqls:
 			if datum['verbalized'] == True:
-    			fo.writelines(json.dumps(datum) + "\n")
+				fo.writelines(json.dumps(datum) + "\n")
 		fo.close()
 
 		questions = 0
 		with open('output/verbalized_template%s_readable.txt' % id,'w+') as output_file:
-    		for datum in data:
-        		try:
-            		output_file.write(datum['verbalized_question'].encode('utf-8')+'\n')
-            		output_file.write(datum['query'].encode('utf-8')+'\n\n')
-            		questions += 1
-        		except:
-            		continue
+			for datum in sparqls:
+				try:
+					output_file.write(datum['verbalized_question'].encode('utf-8')+'\n')
+					output_file.write(datum['query'].encode('utf-8')+'\n\n')
+					questions += 1
+				except:
+					continue
 
-        #Count the number of verbalized questions
-        print "Template ID: ", id
-        print "Generated Questions: ", questions
-		print "Total data items: ", len(data)
+		#Count the number of verbalized questions
+		print "Template ID: ", id
+		print "Generated Questions: ", questions
+		print "Total data items: ", len(sparqls)
 
 	def rules(self, _maps, _datum):
 		'''
@@ -112,5 +128,35 @@ class Verbalizer:
 			Put in all the conditions here which might stop a template from getting verbalized.
 
 			Return type: Boolean
+				Semantics:
+					True: verbalize
+					False: don't verbalize
 		'''
+
+	def hard_relation_filter(self, _pred, _limit = 1):
+		'''
+			Keep the track of one of the predicates appearing in the mapping, and then only allow one question per predicate
+			Uses a class variable (like a global var)
+
+			USAGE: MANUAL! invoke in the filter class if needed
+
+			Params:
+				_pred: type: string. 
+				_limit: the number of questions permitted per relation. Should be more than 1
+
+			Return type: Boolean
+				Semantics:
+					True: verbalize
+					False: don't verbalize
+		'''
+
+		try:
+			if self.hard_relation_filter_map[_pred] >= _limit:
+				return False
+			self.hard_relation_filter_map[_pred] += 1
+		except:
+			self.hard_relation_filter_map[_pred] = 1
+
+		return True
+
 
--- verbalizer_vanilla.py
+++ verbalizer_vanilla.py
@@ -7,3 +7,38 @@
 
 	Finally, a function will create an object of all of them and run it.
 '''
+import numpy as np
+from pprint import pprint
+from pattern.en import pluralize
+import utils.natural_language_utilities as nlutils
+
+import verbalizer
+
+class Verbalizer_03(verbalizer.Verbalizer):
+
+	question_templates = {
+		'vanilla': ["What is the <%(e_in_to_e)s> of the <%(x)s> which is the <%(e_in_in_to_e_in)s> of <%(e_in_in)s> ?"],
+		'person': ["What is the <%(e_in_to_e)s> of the <%(x)s> who is the <%(e_in_in_to_e_in)s> of <%(e_in_in)s> ?"]
+		}
+
+	def filter(self, _datum, _maps):
+		#Just that hard filter thingy
+		return self.hard_relation_filter(_maps['e_in_to_e'])
+
+	def rules(self, _maps, _datum):
+		'''
+			Person Rule. If x has dbo:Agent but not have dbo:Organization.
+				Have to check datum[mapping type] and  not maps
+		'''
+		if 'http://dbpedia.org/ontology/Agent' in _datum['mapping_type']['x'] and "http://dbpedia.org/ontology/Organisation" not in _datum['mapping_type']["x"]:
+			return _maps, np.random.choice(self.question_templates['person'])
+		else:
+			return _maps, np.random.choice(self.question_templates['vanilla'])
+
+
+if __name__ == "__main__":
+
+	template3verbalizer = Verbalizer_03(_template_id = 3, _has_x = True, _template_id_offset = 0, _template_type = "Vanilla")
+
+
+		
