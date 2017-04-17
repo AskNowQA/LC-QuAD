00217b27eb866f4a29ecf24b62322887f8248704
Priyansh Trivedi <pc.priyansh@gmail.com>
2017-04-17 11:31:36 +0200

    All vanilla templates verbalized         Template 3,5,6,7,8,9,15,16 Other changes:         Moved some flags and macros from __init__.py to class variables for better handling in subclasses.         Now URIs are converted to labels AFTER the filtering process. (leads to lesser dropped questions) 	Filter function in verbalizer.py was without a 'pass' statement. Fixed that.

--- verbalizer.py
+++ verbalizer.py
@@ -20,17 +20,24 @@ import utils.natural_language_utilities as nlutils
 
 class Verbalizer:
 
+	'''
+		Class variables. 
+			template_id: the template ID (corresponds to the file from where we pick sparqls. Corresponds to ID in templates.py)
+			has_x: if the x (or x's type) has been generated and expected to be in the mapping. 
+			has_uri: same for URI
+			template type: vanilla (for not count/filter/ask), count, filter_a, filter_b, ask (@TODO: Add a reference of this on the Doc)
+			template id offset: if it's a vanilla then 0, 100 for count... 
+	'''
+	template_id = 0
+	template_type = 'Vanilla'
+	template_id_offset = 0
+	has_x = False
+	has_uri = False
 	question_templates = {}		#Placeholder to keep all the templates here. In the form key: list, key:list
-	
-	def __init__(self, _template_id, _has_x = False, _has_uri = False, _template_id_offset = 0, _template_type = 'Vanilla'):
-		'''
-			Parameters:
-				template_id: the template ID (corresponds to the file from where we pick sparqls. Corresponds to ID in templates.py)
-				has_x: if the x (or x's type) has been generated and expected to be in the mapping. 
-				has_uri: same for URI
-				template type: vanilla (for not count/filter/ask), count, filter_a, filter_b, ask (@TODO: Add a reference of this on the Doc)
-				template id offset: if it's a vanilla then 0, 100 for count... 
 
+	
+	def __init__(self):
+		'''
 			-> Open the template file with this ID. Convert to [JSON,JSON..]
 				-> Pass the JSON from the filter
 				-> Select a template based on rules
@@ -43,7 +50,7 @@ class Verbalizer:
 
 		self.hard_relation_filter_map = {} 		#Keeps count of relations and limits. (Used in filter class)
 
-		with open('sparqls/template%s.txt' % _template_id) as data_file:
+		with open('sparqls/template%s.txt' % self.template_id) as data_file:
 			for line in data_file:
 				sparqls.append(json.loads(line.rstrip('\n')))
 
@@ -56,41 +63,40 @@ class Verbalizer:
 			datum = sparqls[counter]
 			maps = datum["mapping"]
 
-			if _has_uri:
+			if self.has_uri:
 				try:
 					uri = datum["answer_type"]["uri"]
 					maps["uri"] = uri
 				except:
 					continue
 
-			if _has_x:
+			if self.has_x:
 				try:
 					x = datum["answer_type"]["x"]
 					maps["x"] = x
 				except:
 					continue
 
-			#Convert the URIs to their corresponding labels
-			#@TODO: Use dbpedia labels to do this.
-			for element in maps:
-				maps[element] = nlutils.get_label_via_parsing(maps[element], lower = True)  #Get their labels
-
-
 			#See if you want this filtered
 			if not self.filter(_datum = datum, _maps = maps):
 				#Don't bother verbalizing this
 				continue
+
+			#Convert the URIs to their corresponding labels
+			#@TODO: Use dbpedia labels to do this.
+			for element in maps:
+				maps[element] = nlutils.get_label_via_parsing(maps[element], lower = True)  #Get their labels
 
 			#Select a template for this question
 			maps, question_format = self.rules(_maps = maps,_datum = datum)
 
 			sparqls[counter]['verbalized_question'] = question_format % maps
 			sparqls[counter]['verbalized'] = True
-			sparqls[counter]['id'] = _template_id + _template_id_offset
-			sparqls[counter]['type'] = _template_type
+			sparqls[counter]['id'] = self.template_id + self.template_id_offset
+			sparqls[counter]['type'] = self.template_type
 
 		#Writing everything to file
-		id = _template_id + _template_id_offset
+		id = self.template_id + self.template_id_offset
 		fo = open('output/verbalized_template%s.txt' % id, 'w+')
 		for datum in sparqls:
 			if datum['verbalized'] == True:
@@ -110,7 +116,8 @@ class Verbalizer:
 		#Count the number of verbalized questions
 		print "Template ID: ", id
 		print "Generated Questions: ", questions
-		print "Total data items: ", len(sparqls)
+		print "Total data items: ", len(sparqls), 
+		print "\n"
 
 	def rules(self, _maps, _datum):
 		'''
@@ -132,6 +139,7 @@ class Verbalizer:
 					True: verbalize
 					False: don't verbalize
 		'''
+		pass
 
 	def hard_relation_filter(self, _pred, _limit = 1):
 		'''
